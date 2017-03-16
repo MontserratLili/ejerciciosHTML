339b03030cf401bb7f6c52ed8ee9a201ffda1b02
MontserratLili <lauraliligm@gmail.com>
2017-03-16 08:53:35 -0600

    ejerciciosHTML

---
+++ ejercicioEnunciado/convertidor.js
@@ -0,0 +1,11 @@
+function convertirFarenheit(){
+  var a = 0;
+  a = parseInt(document.getElementById('farenheit').value);
+  console.log(a);
+}
+
+function convertirCelsius(){
+var calculo = 0;
+calculo = parseInt(document.getElementById('celsius').value);
+console.log(calculo);
+}
